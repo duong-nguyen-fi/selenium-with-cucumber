6aea549fa36e621889a13ab2e3057c2aa937f819
e1500948 <tynshjt@gmail.com>
2018-09-14 00:02:04 +0300

    Takescreenshot - use remotewebdriver

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -1,11 +1,17 @@
 package steps;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import Base.BaseUtil;
+import Guru99TakeScreenshot.Guru99TakeScreenshot;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by Karthik on 10/17/2016.
@@ -20,17 +26,17 @@ public class Hook extends BaseUtil{
     }
 
     @Before
-    public void InitializeTest() {
+    public void InitializeTest() throws MalformedURLException {
 
-        System.out.println("Opening the browser : Firefox");
-
+        System.out.println("Opening the browser : Chrome");
+        DesiredCapabilities caps = DesiredCapabilities.firefox();
+        caps.setCapability("platform", Platform.LINUX);
         /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
         base.Driver = new FirefoxDriver();*/
-
-
+        
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
-        base.Driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+        base.Driver = new RemoteWebDriver(new URL("http://localhost:3044/wd/hub"),caps);
     }
 
 
@@ -38,6 +44,12 @@ public class Hook extends BaseUtil{
     public void TearDownTest(Scenario scenario) {
         if (scenario.isFailed()) {
             //Take screenshot logic goes here
+        	try {
+				Guru99TakeScreenshot.takeSnapShot(base.Driver, "c://test.png");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}    
             System.out.println(scenario.getName());
         }
         System.out.println("Closing the browser : MOCK");
