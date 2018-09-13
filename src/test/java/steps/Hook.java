51cdb6d5a2cdc66b0524d279dbdfde05b87023d7
e1500948 <tynshjt@gmail.com>
2018-09-14 00:44:21 +0300

    remove platform and version

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -30,9 +30,7 @@ public class Hook extends BaseUtil{
 
         System.out.println("Opening the browser : Firefox");
         DesiredCapabilities caps = DesiredCapabilities.firefox();
-        caps.setCapability("platform", Platform.LINUX);
         caps.setJavascriptEnabled(true);
-        caps.setVersion("61.0.2");
         
         //Chrome driver
         //System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
